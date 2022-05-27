74b9e2b922a60209a13974e1b595f580a238d524
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2022-05-27 11:27:38 +0900

    read lecture file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include "lecture_struct.h"
+#include <fstream>
+#include <vector>
+
 using namespace std;
 
 void readOneModel(Car* data){
@@ -22,23 +25,48 @@ void displayOneModel(Car* data){
 }
 
 int main() {
-    Car car1;
-    car1.Model ="Persona";
-    car1.EngineCapacity =1.6;
-    car1.Price = 60000;
-    Car car2;
-    car2.Model ="Honda City";
-    car2.EngineCapacity =1.5;
-    car2.Price = 89000;
-    Car car3;
-    car3.Model ="Saga";
-    car3.EngineCapacity =1.3;
-    car3.Price = 45000;
-    int totalPrice = car1.Price + car2.Price + car3.Price;
-    cout <<" Total Price: "<< totalPrice << endl;
-    Car * myCar = new Car;
-    readOneModel(myCar);
-    displayOneModel(myCar);
-    return 0;
+//    string filename("lecture.txt");
+    vector<string> lines;
+    string line;
+    const char *path = "/Users/kokubunryuuji/Devspace/dstr-assignment/lecture.txt";
+
+    ifstream ifs(path);
+    if ( !ifs.is_open() ) {
+        cout << "hirakisou?" << endl;
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+        return EXIT_FAILURE;
+    }
+
+    while (getline(ifs, line)){
+        lines.push_back(line);
+    }
+
+    for (const auto &i : lines)
+        cout << i << endl;
+
+    ifs.close();
+    return EXIT_SUCCESS;
+
+
+//    return 0;
+//    Car car1;
+//    car1.Model ="Persona";
+//    car1.EngineCapacity =1.6;
+//    car1.Price = 60000;
+//    Car car2;
+//    car2.Model ="Honda City";
+//    car2.EngineCapacity =1.5;
+//    car2.Price = 89000;
+//    Car car3;
+//    car3.Model ="Saga";
+//    car3.EngineCapacity =1.3;
+//    car3.Price = 45000;
+//    int totalPrice = car1.Price + car2.Price + car3.Price;
+//    cout <<" Total Price: "<< totalPrice << endl;
+//    Car * myCar = new Car;
+//    readOneModel(myCar);
+//    displayOneModel(myCar);
+
 }
 
